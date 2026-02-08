b482d99cf5bf9b185ccb0e8c7aa195ea4feae3d3
dllen <rocnix@duck.com>
2026-02-08 23:12:20 +0800

    build: add algorithm-visualizer build step and update public assets
    Update package.json to include a new build:algo script that builds the algorithm-visualizer library and copies its output to the public directory. This replaces the old static files and updates asset paths from relative (./) to root-relative (/) for proper serving.

--- public/algorithm-visualizer/service-worker.js
+++ public/algorithm-visualizer/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c93ef3140bc109241a6e9b4116f22312.js"
+  "/precache-manifest.2ebdd1d01a91f39526a986638b98fc42.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
